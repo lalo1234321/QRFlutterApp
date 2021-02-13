d9023596cf4d04a0e428bbb4f2a2a03070f5284c
lalo1234321 <edominguezc@toluca.tecnm.mx>
2021-02-12 19:58:48 -0600

    db provider

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:path/path.dart';
+import 'package:qrreaderapp/src/models/scan_model.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -40,7 +41,58 @@ class DBProvider {
 
   // esta forma es para insertar código sql en crudo
   // necesitamos pasar los datos a travez de nuestro modelo y mandar el modelo como parámetro
-  
 
+  nuevoScanRaw(ScanModel nuevoScan) async{
+    // se realiza únicamente una instancia de la base de datos, gracias al patrón singleton
+    final db = await database;
+
+    final res = await db.rawInsert(
+      "INSERT INTO  Scans (id, tipo valor) "
+      "VALUES ( ${nuevoScan.id}, '${nuevoScan.tipo}', '${nuevoScan.valor}')"
+    );
+
+    return res;
+
+
+  }
+
+  nuevoScan( ScanModel nuevoScan ) async{
+    final db = await database;
+    final res = await db.insert('Scans', nuevoScan.toJson());
+    return res;
+  }
+
+  Future<ScanModel> getScanId( int id ) async{
+    final db = await database;
+
+    final res = await db.query('Scans', where: ' id = ? ', whereArgs: [id] );
+
+    // El resultado será entregado como un mapa, y debemos de entregar esta infromacion como si fuera un modelo del scanner
+
+    return res.isNotEmpty ? ScanModel.fromJson(res.first) : null;
+  }
+
+  Future<List<ScanModel>> getScans() async {
+    final db = await database;
+
+    final res = await db.query('Scans');
+
+    List<ScanModel> list = res.isNotEmpty 
+                              ? res.map( (c) => ScanModel.fromJson(c) ).toList()
+                              : [];
+
+    return list;
+  }
+
+  // update
+
+  Future<int> updateScan( ScanModel nuevoScan) async {
+    final db = await database;
+
+    final res = await db.update('Scans', nuevoScan.toJson(), where: ' id = ? ', whereArgs: [nuevoScan.id]);
+
+    // devuelve un 1 si lo hico y un 0 en caso contrario, la cantidad de registros realmente o cambios
+    return res;
+  }
 
 }
